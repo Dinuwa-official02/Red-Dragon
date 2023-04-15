12449d435e32e143f2ceb50a8ff16f3818be05fb
Dinuwa-official02 <126937798+Dinuwa-official02@users.noreply.github.com>
2023-04-15 13:12:47 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -29,14 +29,14 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owner v card
-global.vcardowner = ['+380944814219'] // owner number
-global.ownername = "😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯" // owner name
+global.vcardowner = ['+94717845122'] // owner number
+global.ownername = "😎◎ 𝘿𝙄𝙉𝙐𝙆𝘼࿐🌻🌼 ◎❯❯" // owner name
 global.ytname = "" // yt chanel name
 global.socialm = "GitHub: prabhasha2006" // github or insta name
 global.location = "India, Mizoram, Aizawl" // location
 
 //bot body 
-global.alivepic = `https://i.ibb.co/cyyCjN7/20220803-081426.png` // alive picture \\ use "https://ibb.com" to upload photos. < 1 MB
+global.alivepic = `https://telegra.ph/file/bbee559740a59f5deda3b.jpg` // alive picture \\ use "https://ibb.com" to upload photos. < 1 MB
 
 global.dragonchat = "false"  // chat bot  "true" / "false" (200 auto replies in this)
 global.callblock = "false" // call block "true" / "false" ("true"  to block callers)
@@ -47,13 +47,13 @@ global.upsongvoice = "false" // true/false \\ bot can upload song as voice note 
 global.welcom_msg = "true" // Welcome / Goodbye   [true/false]
 
 
-global.owner = ['+380944814219']
-global.ownertag = '+380944814219' //your tag number
-global.botname = '𝙍𝙀𝘿╺ 𝘿𝙍𝘼𝙂𝙊𝙉 - 𝘉𝘖𝘛' //ur bot name
+global.owner = ['+94717845122']
+global.ownertag = '+94717845122' //your tag number
+global.botname = '𝘿𝙄𝙉𝙐𝙓╺ 𝙊𝙁𝙁𝙄𝘾𝙄𝘼𝙇 - 𝘉𝘖𝘛' //ur bot name
 global.packname = "Bot Sticker" // sticker packname
-global.author = "𝙍𝙀𝘿╺ 𝘿𝙍𝘼𝙂𝙊𝙉" // sticker author
+global.author = "𝘿𝙄𝙉𝙐𝙓╺ 𝙊𝙁𝙁𝙄𝘾𝙄𝘼𝙇" // sticker author
 global.dragonemoji = '🧩' // Menu emoji
-global.footer = '[ ◉ ʀᴇᴅ-ᴅʀᴀɢᴏɴ-ᴍᴅ ]' //
+global.footer = '[ ◉ ᴅɪɴᴜ-ᴏꜰꜰɪᴄɪᴀʟ-ᴍᴅ ]' //
 
 //database
 global.premium = ['+380944814219'] //ur premium numbers
